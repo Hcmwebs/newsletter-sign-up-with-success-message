0abbdc5c2e128618ae2997b46a02f4e0d4c6cd0a
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-01 23:40:22 +1300

    tested the styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
 				/>
 			</figure>
 			<div className='card-body'>
-				<h2 className='card-title'>Shoes!</h2>
+				<h2 className='card-title text-4xl'>Shoes!</h2>
 				<p>If a dog chews shoes whose shoes does he choose?</p>
 				<div className='card-actions justify-end'>
 					<button className='btn btn-primary'>Buy Now</button>
