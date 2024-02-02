d7be8e9e90a1cdb470888b9fe61bb44c5fd75cfa
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-03 01:57:19 +1300

    added the styles for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,43 @@
+import smallScreen from './assets/illustration-sign-up-mobile.svg';
+import largeScreen from './assets/illustration-sign-up-desktop.svg';
 const App = () => {
 	return (
 		<main className='w-[90vw] max-w-[1440px] min-h-screen my-0 mx-auto grid grid-cols-1 place-items-center'>
-			<div className='card w-full max-w-[375px] shadow-xl rounded-none bg-white text-base-100'>
-				<figure>
-					<img
-						src='https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg'
-						alt='Shoes'
-						className='rounded-b-2xl'
-					/>
-				</figure>
-				<div className='card-body'>
-					<h2 className='card-title text-4xl'>Shoes!</h2>
-					<p>If a dog chews shoes whose shoes does he choose?</p>
-					<div className='w-full grid grid-cols-1 items-start'>
-						<ul
-							role='list'
-							className='list-inside list-image-[url(./assets/icon-list.svg)] grid gap-y-3'
-						>
-							<li>Lorem, ipsum dolor.</li>
-							<li>Lorem, ipsum dolor.</li>
-							<li>Lorem, ipsum dolor.</li>
-						</ul>
+			<div className='card w-[375px] lg:w-[928px] lg:h-[641px] lg:flex-row-reverse shadow-xl rounded-none bg-white text-base-100 lg:rounded-2xl lg:p-6 lg:gap-x-16'>
+				<picture>
+					<source media='(max-width:800px)' srcSet={smallScreen} />
+					<source media='(min-width:900px)' srcSet={largeScreen} />
+					<img src={largeScreen} alt='Sign-up' className='rounded-b-2xl' />
+				</picture>
+				<div className='card-body self-center grid grid-col-1 gap-y-10'>
+					<div className='grid grid-cols-1 gap-y-6'>
+						<h2 className='card-title text-4xl'>Shoes!</h2>
+						<p>If a dog chews shoes whose shoes does he choose?</p>
+						<div className='grid grid-cols-1 items-start'>
+							<ul className='list-inside list-image-[url(./assets/icon-list.svg)] grid gap-y-3'>
+								<li>Lorem, ipsum dolor.</li>
+								<li>Lorem, ipsum dolor.</li>
+								<li>Lorem, ipsum dolor.</li>
+							</ul>
+						</div>
 					</div>
-					<form className='grid grid-cols-1 gap-y-6'>
+
+					<form className='grid grid-cols-1 gap-y-6 w-full'>
 						<div className='form-control'>
 							<label htmlFor='email' className='label'>
-								Email
+								Email address
 							</label>
 							<input
 								type='text'
+								placeholder='email@email.com'
 								className='input input-bordered input-base-100 bg-transparent text-base-100 focus:outline-none focus:border-2 focus:border-base-100 invalid:border-primary'
 							/>
 						</div>
 						<div className='card-actions justify-end'>
-							<button className='btn bg-base-100 btn-block hover:bg-base-200 transition duration-300 ease-in-out'>
+							<button
+								type='submit'
+								className='btn bg-base-100 btn-block hover:bg-primary border-0 transition duration-300 ease-in-out'
+							>
 								Submit
 							</button>
 						</div>
