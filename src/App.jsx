10adbec7ac99c9d269643cbcab68898cf6c061e8
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-03 07:33:32 +1300

    fixed the images and card  break points

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import largeScreen from './assets/illustration-sign-up-desktop.svg';
 const App = () => {
 	return (
 		<main className='w-[90vw] max-w-[1440px] min-h-screen my-0 mx-auto grid grid-cols-1 place-items-center'>
-			<div className='card w-[375px] lg:w-[928px] lg:h-[641px] lg:flex-row-reverse shadow-xl rounded-none bg-white text-base-100 lg:rounded-2xl lg:p-6 lg:gap-x-16'>
+			<div className='card w-[375px] max-h-[842px] lg:w-[928px] lg:h-[641px] lg:flex-row-reverse shadow-xl rounded-none bg-white text-base-100 lg:rounded-2xl lg:p-6 lg:gap-x-16'>
 				<picture>
-					<source media='(max-width:800px)' srcSet={smallScreen} />
-					<source media='(min-width:900px)' srcSet={largeScreen} />
+					<source media='(max-width:1000px)' srcSet={smallScreen} />
+					<source media='(min-width:1028px)' srcSet={largeScreen} />
 					<img src={largeScreen} alt='Sign-up' className='rounded-b-2xl' />
 				</picture>
 				<div className='card-body self-center grid grid-col-1 gap-y-10'>
