8da08a5ce6b0ddb810c57f20efef587d248396c9
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-13 12:54:34 +1300

    added span prop to the title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Title = ({ header, text }) => {
+const Title = ({ header, text1, textSpan, text2 }) => {
 	return (
 		<div className='grid grid-cols-1 gap-y-6'>
-			<h1 className='text-4xl'>{header}</h1>
-			<p className='text-base'>{text}</p>
+			<h1 className='text-4xl lg:text-5xl font-bold'>{header}</h1>
+			<p className='text-base font-normal'>
+				{text1}
+				<b>{textSpan}</b>
+				{text2}
+			</p>
 		</div>
 	);
 };
