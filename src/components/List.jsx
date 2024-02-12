705248255610e30b164c64a521e1c1f788167b63
Heze <64218603+Hcmwebs@users.noreply.github.com>
2024-02-12 22:06:33 +1300

    fixed a typo

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const List = () => {
 	return (
 		<div className='grid grid-cols-1 items-start '>
-			<ul className='grid  items-start list-inside list-image-[url(./assets/icon-list.svg)] gap-y-3'>
+			<ul className='grid items-start list-inside list-image-[url(./assets/icon-list.svg)] gap-y-3'>
 				<li>Lorem, ipsum dolor.</li>
 				<li>Lorem, ipsum dolor.</li>
 				<li>Lorem, ipsum dolor.</li>
